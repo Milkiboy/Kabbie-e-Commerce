f993fc356949fbe8c71d49e80e9de5b342715020
Milkias Mebrahru <milkiboy2016@gmail.com>
2023-03-23 17:31:19 -0500

    test

--- src/main/java/com/kabbeeecommercebackend/ECommerceBackendApplication.java
+++ src/main/java/com/kabbeeecommercebackend/ECommerceBackendApplication.java
@@ -8,6 +8,8 @@ public class ECommerceBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceBackendApplication.class, args);
+
+		System.out.println("test");
 	}
 
 }
